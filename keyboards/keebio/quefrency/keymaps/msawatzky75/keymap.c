8a5d802a8b06c12184f771819604dba2615b289b
Matthew Sawatzky <msawatzky75@gmail.com>
2021-12-27 02:28:15 -0600

    couple more tweaks

--- keyboards/keebio/quefrency/keymaps/msawatzky75/keymap.c
+++ keyboards/keebio/quefrency/keymaps/msawatzky75/keymap.c
@@ -19,19 +19,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ├───────────┼───────────┼─────────────────┴──┬────────┴──┬────────┴──┬────────┴──┬────────┴──┬────────┴──┐         └──┬────────┴──┬────────┴──┬────────┴──┬────────┴──┬────────┴──┬────────┴──┬────────┴─────────────────┼───────────┤
     TO(_QWERTY),MO(_MISC)  ,KC_BSPC             ,KC_A       ,KC_O       ,KC_E       ,KC_U       ,KC_I       ,             KC_D       ,KC_H       ,KC_T       ,KC_N       ,KC_S       ,KC_MINS    ,KC_ENT                    ,KC_PGUP    ,
 // ├───────────┼───────────┼────────────────────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┐      └─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴──────────────┬───────────┼───────────┤
-    TO(_COLEMAK)  ,MO(_MISC)  ,KC_LSFT                   ,KC_SCLN    ,KC_Q       ,KC_J       ,KC_K       ,KC_X       ,             KC_B       ,KC_M       ,KC_W       ,KC_V       ,KC_Z       ,KC_RSFT             ,KC_UP      ,KC_PGDN    ,
+    TO(_COLEMAK),MO(_MISC) ,KC_LSFT                   ,KC_SCLN    ,KC_Q       ,KC_J       ,KC_K       ,KC_X       ,             KC_B       ,KC_M       ,KC_W       ,KC_V       ,KC_Z       ,KC_RSFT             ,KC_UP      ,KC_PGDN    ,
 // ├───────────┼───────────┼──────────────┬───────────┴──┬────────┴─────┬─────┴────────┬──┴───────────┴───────────┘            ├───────────┴───────────┴────────┬──┴────────┬──┴────────┬──┴────────┬───────────┼───────────┼───────────┤
-    MO(_MISC)  ,MO(_MISC)  ,KC_LCTL       ,KC_LGUI       ,KC_LALT       ,KC_SPC        ,KC_SPC                    ,             MO(_MISC),KC_SPC                ,KC_RCTL    ,KC_RALT    ,KC_RGUI    ,KC_LEFT    ,KC_DOWN    ,KC_RGHT
+    MO(_MISC)  ,MO(_MISC)  ,KC_LCTL       ,KC_LGUI       ,KC_LALT       ,KC_SPC        ,KC_SPC                    ,             KC_BSPC,KC_BSPC                 ,KC_RCTL    ,KC_RALT    ,KC_RGUI    ,KC_LEFT    ,KC_DOWN    ,KC_RGHT
 // └───────────┴───────────┴──────────────┴──────────────┴──────────────┴──────────────┴──────────────────────────┘            └────────────────────────────────┴───────────┴───────────┴───────────┴───────────┴───────────┴───────────┘
   ),
 
   [_QWERTY] = LAYOUT_65_with_macro(
 // ┌───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┐            ┌───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┐
-    KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,             KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_MINS    ,KC_EQL     ,KC_NO      ,KC_BSPC    ,KC_DEL     ,
+    KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,             KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_MINS    ,KC_EQL     ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,
 // ├───────────┼───────────┼───────────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┘      ┌─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴───────────┼───────────┤
-    KC_TRNS    ,KC_TRNS    ,KC_TRNS          ,KC_Q       ,KC_W       ,KC_E       ,KC_R       ,KC_T       ,             KC_Y       ,KC_U       ,KC_I       ,KC_O       ,KC_P       ,KC_LBRC    ,KC_RBRC    ,KC_BSLS          ,KC_HOME    ,
+    KC_TRNS    ,KC_TRNS    ,KC_TRNS          ,KC_Q       ,KC_W       ,KC_E       ,KC_R       ,KC_T       ,             KC_Y       ,KC_U       ,KC_I       ,KC_O       ,KC_P       ,KC_LBRC    ,KC_RBRC    ,KC_BSLS          ,KC_TRNS    ,
 // ├───────────┼───────────┼─────────────────┴──┬────────┴──┬────────┴──┬────────┴──┬────────┴──┬────────┴──┐         └──┬────────┴──┬────────┴──┬────────┴──┬────────┴──┬────────┴──┬────────┴──┬────────┴─────────────────┼───────────┤
-    KC_TRNS    ,KC_TRNS    ,KC_TRNS             ,KC_A       ,KC_S       ,KC_D       ,KC_F       ,KC_G       ,             KC_H       ,KC_J       ,KC_K       ,KC_L       ,KC_SCLN    ,KC_QUOT    ,KC_ENT                    ,KC_END     ,
+    KC_TRNS    ,KC_TRNS    ,KC_TRNS             ,KC_A       ,KC_S       ,KC_D       ,KC_F       ,KC_G       ,             KC_H       ,KC_J       ,KC_K       ,KC_L       ,KC_SCLN    ,KC_QUOT    ,KC_ENT                    ,KC_TRNS    ,
 // ├───────────┼───────────┼────────────────────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┐      └─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴──────────────┬───────────┼───────────┤
     KC_TRNS    ,KC_TRNS    ,KC_TRNS                   ,KC_Z       ,KC_X       ,KC_C       ,KC_V       ,KC_B       ,             KC_N       ,KC_M       ,KC_COMM    ,KC_DOT     ,KC_SLASH   ,KC_RSFT             ,KC_UP      ,KC_TRNS    ,
 // ├───────────┼───────────┼──────────────┬───────────┴──┬────────┴─────┬─────┴────────┬──┴───────────┴───────────┘            ├───────────┴───────────┴────────┬──┴────────┬──┴────────┬──┴────────┬───────────┼───────────┼───────────┤
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_COLEMAK] = LAYOUT_65_with_macro(
 // ┌───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┐            ┌───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┐
-    KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,             KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_MINS    ,KC_EQL     ,KC_NO      ,KC_BSPC    ,KC_DEL     ,
+    KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,             KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_MINS    ,KC_EQL     ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,
 // ├───────────┼───────────┼───────────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┘      ┌─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴───────────┼───────────┤
     KC_TRNS    ,KC_TRNS    ,KC_TRNS          ,KC_Q       ,KC_W       ,KC_F       ,KC_P       ,KC_G       ,             KC_J       ,KC_L       ,KC_U       ,KC_Y       ,KC_SCLN    ,KC_LBRC    ,KC_RBRC    ,KC_BSLS          ,KC_TRNS    ,
 // ├───────────┼───────────┼─────────────────┴──┬────────┴──┬────────┴──┬────────┴──┬────────┴──┬────────┴──┐         └──┬────────┴──┬────────┴──┬────────┴──┬────────┴──┬────────┴──┬────────┴──┬────────┴─────────────────┼───────────┤
@@ -49,21 +49,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ├───────────┼───────────┼────────────────────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┐      └─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴──────────────┬───────────┼───────────┤
     KC_TRNS    ,KC_TRNS    ,KC_TRNS                   ,KC_Z       ,KC_X       ,KC_C       ,KC_V       ,KC_B       ,             KC_K       ,KC_M       ,KC_COMM    ,KC_DOT     ,KC_SLASH   ,KC_TRNS             ,KC_TRNS    ,KC_TRNS    ,
 // ├───────────┼───────────┼──────────────┬───────────┴──┬────────┴─────┬─────┴────────┬──┴───────────┴───────────┘            ├───────────┴───────────┴────────┬──┴────────┬──┴────────┬──┴────────┬───────────┼───────────┼───────────┤
-    KC_TRNS    ,KC_TRNS    ,KC_TRNS       ,KC_TRNS       ,KC_TRNS       ,KC_TRNS       ,KC_TRNS                   ,             KC_TRNS,KC_TRNS                       ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS
+    KC_TRNS    ,KC_TRNS    ,KC_TRNS       ,KC_TRNS       ,KC_TRNS       ,KC_TRNS       ,KC_TRNS                   ,             KC_TRNS,KC_TRNS                 ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS
 // └───────────┴───────────┴──────────────┴──────────────┴──────────────┴──────────────┴──────────────────────────┘            └────────────────────────────────┴───────────┴───────────┴───────────┴───────────┴───────────┴───────────┘
   ),
 
   [_MISC] = LAYOUT_65_with_macro(
 // ┌───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┐            ┌───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┐
-    KC_GESC    ,KC_F1      ,KC_F2      ,KC_F3      ,KC_F4      ,KC_F5      ,KC_F6      ,KC_F7      ,KC_F8      ,             KC_F9      ,KC_F10     ,KC_F11     ,KC_F12     ,KC_DEL     ,KC_BSPC    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,
+    KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_F1      ,KC_F2      ,KC_F3      ,KC_F4      ,KC_F5      ,KC_F6      ,             KC_F7      ,KC_F8      ,KC_F9      ,KC_F10     ,KC_F11     ,KC_F12     ,KC_TRNS    ,KC_DEL     ,KC_PSCR    ,
 // ├───────────┼───────────┼───────────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┘      ┌─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴───────────┼───────────┘
-    KC_TRNS    ,KC_TRNS    ,KC_TAB           ,KC_QUOT    ,KC_COMM    ,KC_DOT     ,KC_P       ,KC_Y       ,             KC_F       ,KC_G       ,KC_C       ,KC_R       ,KC_L       ,KC_SLSH    ,KC_EQL     ,KC_BSLS          ,KC_END     ,
+    KC_TRNS    ,KC_TRNS    ,KC_TRNS          ,KC_TRNS    ,KC_TRNS    ,KC_TRNS     ,KC_TRNS   ,KC_TRNS    ,             KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS          ,KC_TRNS    ,
 // ├───────────┼───────────┼─────────────────┴──┬────────┴──┬────────┴──┬────────┴──┬────────┴──┬────────┴──┐         └──┬────────┴──┬────────┴──┬────────┴──┬────────┴──┬────────┴──┬────────┴──┬────────┴─────────────────┼───────────┤
-    KC_TRNS    ,KC_TRNS    ,KC_BSPC             ,KC_A       ,KC_O       ,KC_E       ,KC_U       ,KC_I       ,             KC_D       ,KC_H       ,KC_T       ,KC_N       ,KC_S       ,KC_SCLN    ,KC_ENT                    ,KC_PGUP    ,
+    KC_TRNS    ,KC_TRNS    ,KC_TRNS             ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,             KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS                   ,KC_TRNS    ,
 // ├───────────┼───────────┼────────────────────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┐      └─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴─────┬─────┴──────────────┬───────────┼───────────┤
-    KC_TRNS    ,KC_TRNS    ,KC_LSFT                   ,KC_SCLN    ,KC_Q       ,KC_J       ,KC_K       ,KC_X       ,             KC_B       ,KC_M       ,KC_W       ,KC_V       ,KC_Z       ,KC_RSFT             ,KC_UP      ,KC_TRNS    ,
+    KC_TRNS    ,KC_TRNS    ,KC_TRNS                   ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,             KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS             ,KC_TRNS    ,KC_TRNS    ,
 // ├───────────┼───────────┼──────────────┬───────────┴──┬────────┴─────┬─────┴────────┬──┴───────────┴───────────┘            ├───────────┴───────────┴────────┬──┴────────┬──┴────────┬──┴────────┬───────────┼───────────┼───────────┤
-    KC_TRNS    ,KC_TRNS    ,KC_LCTL       ,KC_LGUI       ,KC_LALT       ,KC_TRNS       ,KC_SPC                    ,             KC_TRNS,KC_TRNS                 ,KC_RCTL    ,KC_RALT    ,KC_RGUI    ,KC_LEFT    ,KC_DOWN    ,KC_RGHT
+    KC_TRNS    ,KC_TRNS    ,KC_TRNS       ,KC_TRNS       ,KC_TRNS       ,KC_TRNS       ,KC_TRNS                   ,             KC_DEL,KC_DEL                   ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS
 // └───────────┴───────────┴──────────────┴──────────────┴──────────────┴──────────────┴──────────────────────────┘            └────────────────────────────────┴───────────┴───────────┴───────────┴───────────┴───────────┴───────────┘
   ),
 };
@@ -83,5 +83,5 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
             tap_code(KC_PGUP);
         }
     }
-    return false;
+    return true;
 }
